18b4516e4ad82ecbc6964829b699c27fb02356b1
Jack van Zyl <jackvanzyl@icloud.com>
2019-03-28 02:04:48 +0200

    Make the config work with Ionic

--- cordova-js-src/confighelper.js
+++ cordova-js-src/confighelper.js
@@ -64,7 +64,7 @@ function readConfig (success, error) {
     xhr.addEventListener('load', xhrStatusChangeHandler);
 
     try {
-        xhr.open('get', '/config.xml', true);
+        xhr.open('get', 'config.xml', true);
         xhr.send();
     } catch (e) {
         fail('[Electron][cordova.js][readConfig] Could not XHR config.xml: ' + JSON.stringify(e));
