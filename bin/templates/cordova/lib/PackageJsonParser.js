1c436173bca2d332608f378d43bdc21979dbeb7f
エリス <erisu@users.noreply.github.com>
2019-05-27 21:00:14 +0900

    Update bin/templates/cordova/lib/PackageJsonParser.js
    Co-Authored-By: Jan Piotrowski <piotrowski+github@gmail.com>

--- bin/templates/cordova/lib/PackageJsonParser.js
+++ bin/templates/cordova/lib/PackageJsonParser.js
@@ -46,7 +46,7 @@ class PackageJsonParser {
 \t- ${cordovaDependencies.join('\n\t- ')}
 
 It is recommended that all Cordova packages are defined as "devDependencies" in the "package.json" file. It is safe to move them manually.
-Note: Defined package as a dependency will be bundled with the application and can increase the built application's size greatly.
+Packages defined as a dependency will be bundled with the application and can increase the built application's size.
 `);
             }
 
