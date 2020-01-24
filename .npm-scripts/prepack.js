c016372b546cacca0bef0d180673da5a277e8445
Erisu <ellis.bryan@gmail.com>
2020-01-24 11:35:34 +0900

    fix(npm-script): prepack

--- .npm-scripts/prepack.js
+++ .npm-scripts/prepack.js
@@ -1,5 +1,5 @@
 const { resolve } = require('path');
-const { copyFileSync } = require('fs-extra');
+const { copyFileSync } = require('fs');
 
 // copy cordova.js to bin/templates/project
 copyFileSync(resolve('cordova-lib/cordova.js'), resolve('bin/templates/platform_www/cordova.js'));
