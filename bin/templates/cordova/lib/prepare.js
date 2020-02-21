48864c78e9396314be2ba39222987f6312dc6470
Erisu <ellis.bryan@gmail.com>
2020-02-21 22:51:51 +0900

    refactor (PackageJsonParser.js): relocate to package root
    * update tests and paths
    * relocate test spec to match pathing

--- bin/templates/cordova/lib/prepare.js
+++ bin/templates/cordova/lib/prepare.js
@@ -21,7 +21,7 @@ const fs = require('fs-extra');
 const path = require('path');
 const { ConfigParser, xmlHelpers, events, CordovaError } = require('cordova-common');
 const ManifestJsonParser = require('../../../../lib/ManifestJsonParser');
-const PackageJsonParser = require('./PackageJsonParser');
+const PackageJsonParser = require('../../../../lib/PackageJsonParser');
 const SettingJsonParser = require('../../../../lib/SettingJsonParser');
 
 module.exports.prepare = function (cordovaProject, options) {
