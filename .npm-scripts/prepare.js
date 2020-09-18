682edee5ef09a5132dd134de8460a2ddf269df33
Raphael von der Grün <raphinesse@gmail.com>
2020-09-18 16:09:43 +0200

    build: build cordova.js on prepare

--- .npm-scripts/prepare.js
+++
@@ -1,5 +0,0 @@
-const { resolve } = require('path');
-const { copyFileSync } = require('fs');
-
-// copy cordova.js to bin/templates/project
-copyFileSync(resolve('cordova-lib/cordova.js'), resolve('bin/templates/platform_www/cordova.js'));
